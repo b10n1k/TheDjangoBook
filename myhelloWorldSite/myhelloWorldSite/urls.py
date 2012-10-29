33c639c5b0a3dd2308bae3a9fdaf565475dbdfdb
j0ny <jbonatakis@gmail.com>
2012-10-29 12:39:36 +0200

    project from ch1/2/3

---
+++ myhelloWorldSite/myhelloWorldSite/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from myhelloWorldSite.views import hello, current_datetime, hours_ahead
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+('^hello/$',hello),
+('^time/$', current_datetime),
+('^time/plus/(\d{1,2})/$',hours_ahead)
+)
---
+++ myhelloWorldSite/myhelloWorldSite/views.py
@@ -0,0 +1,23 @@
+from django.template.loader import get_template
+from django.template import Context
+from django.http import Http404,HttpResponse
+from django.shortcuts import render_to_response
+import datetime
+
+def hello(request):
+    return HttpResponse("Hello World")
+
+def current_datetime(request):
+    now=datetime.datetime.now()
+
+    return render_to_response('current_datetime.html',{'current_date':now})
+
+def hours_ahead(request,offset):
+    try:
+        offset=int(offset)
+    except ValueError:
+        raise Http404()
+    dt=datetime.datetime.now()+datetime.timedelta(hours=offset)
+    assert False
+    html="<html><body>In %s hours(s), it will be %s .</body></html>" %(offset, dt)
+    return HttpResponse(html)
