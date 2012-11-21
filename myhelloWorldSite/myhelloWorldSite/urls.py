783d4a99c5beef3fac68b922b14d943c917bbd3e
j0ny <jbonatakis@gmail.com>
2012-11-21 19:26:31 +0200

    ch6,7

--- myhelloWorldSite/myhelloWorldSite/urls.py
+++ myhelloWorldSite/myhelloWorldSite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from myhelloWorldSite.views import hello, current_datetime, hours_ahead
+from books import views
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -9,5 +10,6 @@ urlpatterns = patterns('',
 ('^hello/$',hello),
 ('^time/$', current_datetime),
 ('^time/plus/(\d{1,2})/$',hours_ahead),
-
+(r'^search_form/$',views.search_form),
+(r'^search/$',views.search),
 )
